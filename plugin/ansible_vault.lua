f72e7dd789a3d20db4e6c9a8fe607344786b3477
Imogen Koch <imogen@imogenthe.dev>
2023-10-03 15:17:39 -0600

    initial commit

---
+++ plugin/ansible_vault.lua
@@ -0,0 +1,16 @@
+-- Title:        ansible-vault.nvim
+-- Description:  ansible-vault encrypting and decrypting within nvim
+-- Last Change:  3 October 2023
+-- Maintainer:   imogen-ooo <https://github.com/imogen-ooo>
+
+local api = vim.api
+
+-- Prevents the plugin from being loaded multiple times. If the loaded
+-- variable exists, do nothing more. Otherwise, assign the loaded
+-- variable and continue running this instance of the plugin.
+
+if vim.g.loaded_ansible_vault == 1 then
+  return
+end
+vim.g.loaded_ansible_vault = 1
+
